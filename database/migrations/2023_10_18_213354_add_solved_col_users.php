324701fee95e28fb6e5a56013e1712633050d86f
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2023-10-18 22:44:02 +0100

    Add solved column to users table

---
+++ database/migrations/2023_10_18_213354_add_solved_col_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Query\Expression;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->jsonb('solved')->default(new Expression('\'{"challenges": [], "questions": []}\'::json'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('solved');
+        });
+    }
+};
--- resources/views/challenges/show.blade.php
+++ resources/views/challenges/show.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12" id="math">
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="w-full">
@@ -22,7 +22,7 @@
                     </div>
                     <div class="mt-6">
                         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                            {{ __('Questions') }} 
+                            {{ __('Questions') }}
                         </h2>
                     @foreach ($challenge->questions as $question)
                         <livewire:challenges.question :challenge="$challenge" :question="$question" />
