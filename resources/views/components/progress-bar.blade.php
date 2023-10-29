ef5a7816f553f6abec4f6a1cf5f84d2fec6b584b
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2023-10-29 21:09:28 +0000

    add progress-bar component

---
+++ resources/views/components/progress-bar.blade.php
@@ -0,0 +1,5 @@
+@props(['percentage'])
+
+<div {{ $attributes->merge(['class' => 'w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700']) }}>
+  <div class="bg-blue-600 h-2.5 rounded-full" style="width: {{ $percentage }}%"></div>
+</div>
