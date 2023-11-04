c2095e961d5e49373efa8cea87780f7043563436
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2023-11-04 17:23:29 +0000

    Disable markdown font

--- resources/views/components/mmd.blade.php
+++ resources/views/components/mmd.blade.php
@@ -1,3 +1,3 @@
-<div class="markdown-body" x-init="$nextTick(() => { MathJax.typeset() });">
+<div class="markdown-body font-medium text-gray-700" x-init="$nextTick(() => { MathJax.typeset() });">
     {!! Illuminate\Support\Str::markdown($slot, ['html_input' => 'strip']) !!}
 </div>
