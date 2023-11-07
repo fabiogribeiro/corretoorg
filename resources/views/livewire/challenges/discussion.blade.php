f69fb3a0e0d1da5cc8d8626924f1c0904f0f798f
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2023-11-07 22:42:07 +0000

    comments style

--- resources/views/livewire/challenges/discussion.blade.php
+++ resources/views/livewire/challenges/discussion.blade.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 use Livewire\Volt\Component;
 use Illuminate\Database\Eloquent\Collection;
@@ -28,10 +28,18 @@ new class extends Component
 
 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg space-y-6">
     <h2 class="text-2xl font-bold text-gray-700">{{ __('Discussion') }}</h2>
-    <ul>
+    <ul class="divide-y divide-gray-100">
     @foreach($comments as $comment)
-        <li>
-            {{ $comment->text }}
+        <li class="flex justify-between items-center py-3">
+            <div class="shrink-0">
+                <p class="text-justify font-semibold text-gray-900 max-w-prose">
+                    {{ $comment->text }}
+                </p>
+                <p class="mt-1 text-sm text-gray-500">{{ $comment->author->name }}</p>
+            </div>
+            <div>
+                <p class="text-gray-500 text-sm">{{ $comment->created_at->format('h:i, d-m-Y') }}</p>
+            </div>
         </li>
     @endforeach
     </ul>
