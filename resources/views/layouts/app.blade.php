0834aed648b9230d1f272d3a4ee5ff529120d4d3
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2024-02-06 19:40:46 +0000

    Add simple favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ $title ?? 'Title' }} - correto.org</title>
 
+        <link rel="icon" type="image/png" href="favicon-32x32.png">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
