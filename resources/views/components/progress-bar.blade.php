000f9b4ee91b398c2c351efb7ed587d6467dea6a
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2024-06-17 21:10:56 +0100

    Different progress bar color when done

--- resources/views/components/progress-bar.blade.php
+++ resources/views/components/progress-bar.blade.php
@@ -1,3 +1,3 @@
 <div {{ $attributes->merge(['class' => 'w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700']) }}>
-  <div class="bg-blue-600 h-2.5 rounded-full" :style="'width: ' + current/total*100 + '%'"></div>
+  <div class="bg-blue-600 h-2.5 rounded-full" :class="current === total && 'bg-emerald-500'" :style="'width: ' + current/total*100 + '%'"></div>
 </div>
