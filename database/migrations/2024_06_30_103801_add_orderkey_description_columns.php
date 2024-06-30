556685ee3696870a4982475c880e61a9d816697c
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2024-06-30 12:02:36 +0100

    add description and order_key cols

---
+++ database/migrations/2024_06_30_103801_add_orderkey_description_columns.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('challenges', function (Blueprint $table) {
+            $table->string('description')->default('');
+            $table->integer('order_key')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('challenges', function (Blueprint $table) {
+            $table->dropColumn(['description', 'order_key',]);
+        });
+    }
+};
