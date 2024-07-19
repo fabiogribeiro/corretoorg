e8c107f9e569f3ce8c2b5545fd0cb603f8c74d20
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2024-07-19 19:41:39 +0100

    consistent styles

--- resources/views/livewire/challenges/question-multiple-choice.blade.php
+++ resources/views/livewire/challenges/question-multiple-choice.blade.php
@@ -40,7 +40,7 @@ new class extends Component
         @else
         <div class="mt-6 space-y-3">
             <div class="flex flex-col w-72">
-                <select wire:model="answer" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 {{ $submitted? 'ring-1 border-red-400 ring-red-400' : '' }}">
+                <select wire:model="answer" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 @forelse ($question->answer_data['options'] as $op)
                     <option value="{{ $op }}">{{ $op }}</option>
                 @empty
