cf55a58755b4e190b70b5d135da670c14c5c3edc
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2024-08-15 19:19:57 +0100

    pest, pkg upgrades

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,37 +1,28 @@
 <?php
 
-namespace Tests\Feature\Auth;
-
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Volt\Volt;
-use Tests\TestCase;
 
-class RegistrationTest extends TestCase
-{
-    use RefreshDatabase;
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-    public function test_registration_screen_can_be_rendered(): void
-    {
-        $response = $this->get('/register');
+test('registration screen can be rendered', function () {
+    $response = $this->get('/register');
 
-        $response
-            ->assertSeeVolt('pages.auth.register')
-            ->assertOk();
-    }
+    $response
+        ->assertSeeVolt('pages.auth.register')
+        ->assertOk();
+});
 
-    public function test_new_users_can_register(): void
-    {
-        $component = Volt::test('pages.auth.register')
-            ->set('name', 'Test User')
-            ->set('email', 'test@example.com')
-            ->set('password', 'password')
-            ->set('password_confirmation', 'password');
+test('new users can register', function () {
+    $component = Volt::test('pages.auth.register')
+        ->set('name', 'Test User')
+        ->set('email', 'test@example.com')
+        ->set('password', 'password')
+        ->set('password_confirmation', 'password');
 
-        $component->call('register');
+    $component->call('register');
 
-        $component->assertRedirect(RouteServiceProvider::HOME);
+    $component->assertRedirect(RouteServiceProvider::HOME);
 
-        $this->assertAuthenticated();
-    }
-}
+    $this->assertAuthenticated();
+});
