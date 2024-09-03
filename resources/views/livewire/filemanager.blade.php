5b96105259802dc5b592ceaae2b711697b06f5ca
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2024-09-03 20:06:44 +0100

    drop files into dropzone

--- resources/views/livewire/filemanager.blade.php
+++ resources/views/livewire/filemanager.blade.php
@@ -43,7 +43,19 @@ new #[Layout('layouts.app')] class extends Component
 }; ?>
 
 <div
-    x-data="{ uploading: false, current: 0, total: 100 }"
+    x-data="{
+        uploading: false,
+        current: 0,
+        total: 100,
+        handleDrop(e) {
+            e.preventDefault();
+            const fileInput = document.getElementById('dropzone-file');
+            if (e.dataTransfer.files.length > 0) {
+                fileInput.files = e.dataTransfer.files;
+                fileInput.dispatchEvent(new Event('change'));
+            }
+        }
+    }"
     x-on:livewire-upload-start="uploading = true"
     x-on:livewire-upload-finish="uploading = false"
     x-on:livewire-upload-cancel="uploading = false"
@@ -54,7 +66,7 @@ new #[Layout('layouts.app')] class extends Component
 
     <form wire:submit="save" class="py-6 max-w-6xl mx-auto">
         <h1 class="text-3xl font-bold">{{ __('Files') }}</h1>
-        <div class="mt-9 flex flex-col">
+        <div class="my-6 flex flex-col">
         @foreach ($files as $file)
             <div class="my-3 flex items-center space-x-3">
                 <img src="{{ $file->temporaryUrl() }}" class="max-w-24">
@@ -68,7 +80,10 @@ new #[Layout('layouts.app')] class extends Component
 
 
         <div class="flex items-center justify-center w-full">
-            <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+            <label for="dropzone-file"
+                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                    x-on:dragover.prevent
+                    x-on:drop="handleDrop">
                 <div class="flex flex-col items-center justify-center pt-5 pb-6">
                     <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
