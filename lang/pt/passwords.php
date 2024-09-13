7207aaa03b706908538c41b325f27dc54303beba
Fábio Ribeiro <fabio97ribeiro@hotmail.com>
2024-09-13 23:10:19 +0100

    Add some email translations

--- lang/pt/passwords.php
+++ lang/pt/passwords.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
+
+    'reset' => 'A sua palavra-passe foi redefinida.',
     'sent' => 'Enviamos o link para redefinir a palavra-passe.',
+    'user' => "Não encontramos um utilizador com esse endereço de email.",
+
 ];
